70306649e6aec8b8c108fba409d7dcc72cbda3af
Ranveer225 <124071847+Ranveer225@users.noreply.github.com>
2023-11-13 18:42:50 -0500

    Implement fields for reasonForVisit and Symptoms

---
+++ src/main/java/com/example/medicalassistantjavafx/SpeakToDoctor.java
@@ -0,0 +1,24 @@
+package com.example.medicalassistantjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SpeakToDoctor {
+
+    @FXML
+    private TextField ReasonForVisitField;
+
+    @FXML
+    private TextField SymptomsField;
+
+   private TextField DiagnosticField;
+
+    @FXML
+    private void handleSpeakToDoctor() {
+        String ReasonForVisit = ReasonForVisitField.getText().trim();
+        String Symptoms = SymptomsField.getText().trim();
+        String Diagnostic = DiagnosticField.getText().trim();
+
+        // Reason for Visit and Symptoms variable
+    }
+}
