4f8c3ff8287bccaf68108f6201e9222672ee5f82
Ranveer225 <124071847+Ranveer225@users.noreply.github.com>
2023-11-13 18:48:59 -0500

    Add in last field for lab report

--- src/main/java/com/example/medicalassistantjavafx/SpeakToDoctor.java
+++ src/main/java/com/example/medicalassistantjavafx/SpeakToDoctor.java
@@ -14,11 +14,13 @@ public class SpeakToDoctor {
    private TextField DiagnosticField;
 
     @FXML
+    private TextField labReportField;
+    @FXML
     private void handleSpeakToDoctor() {
         String ReasonForVisit = ReasonForVisitField.getText().trim();
         String Symptoms = SymptomsField.getText().trim();
         String Diagnostic = DiagnosticField.getText().trim();
-
+        String labReport = labReportField.getText().trim();
 
         // Reason for Visit and Symptoms variable
     }
